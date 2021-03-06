2ba1ad8eaad5e79f1b1c77ab2d827982728b9d88
LinuxPanda <LinuxPanda@users.noreply.github.com>
2021-03-06 16:50:39 +0530

    rosy: update copyrights to 2021, fix whitespaces

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 The Android Open Source Project
+# Copyright 2021 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +17,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE), rosy)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+    include $(call all-makefiles-under, $(LOCAL_PATH))
 endif
